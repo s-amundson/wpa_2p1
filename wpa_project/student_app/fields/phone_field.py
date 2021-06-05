99ebc7b27579f131ade6b2b5e38072410afc7889
sam <sam.amundson@gmail.com>
2021-06-05 07:08:41 -0700

    updates to phone field and replace_db

--- wpa_project/student_app/fields/phone_field.py
+++ wpa_project/student_app/fields/phone_field.py
@@ -4,7 +4,7 @@ from django.db import models
 import phonenumbers
 logger = logging.getLogger(__name__)
 
-
+# TODO: format output string for templates - not sure where to perform formatting
 class PhoneField(models.CharField):
     def __init__(self,  *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,3 +17,11 @@ class PhoneField(models.CharField):
             s = phonenumbers.format_number(n, phonenumbers.PhoneNumberFormat.E164)
             logging.debug(f'n = {n}, s = {s}')
             return s
+        if value is None:
+            return value
+
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        n = phonenumbers.format_number(value, phonenumbers.PhoneNumberFormat.NATIONAL)
+        logging.debug(f'unformatted phone number: {value}, formatted: n')
+        return n
