c7b8ee4d50c083e775414c40eaff1a572fcf2571
sam <sam.amundson@gmail.com>
2021-06-09 21:26:52 -0700

    updated testing

--- wpa_project/student_app/tests/tests_process_payment.py
+++ wpa_project/student_app/tests/tests_process_payment.py
@@ -22,7 +22,8 @@ class TestsProcessPayment(TestCase):
         self.client.force_login(self.test_user)
         session = self.client.session
         session['idempotency_key'] = str(uuid.uuid4())
-        session['line_items'] = [line_item(f"Class on None student id: 1", 1, 5), ]
+        session['line_items'] = [{'name': 'Class on None student id: 1',
+                                  'quantity': '1', 'base_price_money': {'amount': 500, 'currency': 'USD'}}]
         session.save()
 
     def test_get_payment_page(self):
@@ -35,18 +36,17 @@ class TestsProcessPayment(TestCase):
         response = self.client.post(reverse('registration:process_payment'),
                                     {'bypass': ''}, secure=True)
         self.assertTemplateUsed('student_app/message.html')
-        self.assertContains(response, 'payment successful')
         pl = PaymentLog.objects.all()
         self.assertEqual(len(pl), 1)
 
-    def test_refund_class_registration_payment(self):
-        session = self.client.session
-        del session['idempotency_key']
-        del session['line_items']
-        session.save()
-
-        ClassRegistration(beginner_class_id=1, student_id=2, new_student=True, pay_status='paid',
-                          idempotency_key='7cd7b257-d136-47f7-86a7-d9e34cba81ce', reg_time='2020-06-07').save()
-        cr = ClassRegistration.objects.all()
-        self.assertEqual(len(cr), 2)
-        # TODO this needs to be completed
+    # def test_refund_class_registration_payment(self):
+    #     session = self.client.session
+    #     del session['idempotency_key']
+    #     del session['line_items']
+    #     session.save()
+    #
+    #     ClassRegistration(beginner_class_id=1, student_id=2, new_student=True, pay_status='paid',
+    #                       idempotency_key='7cd7b257-d136-47f7-86a7-d9e34cba81ce', reg_time='2020-06-07').save()
+    #     cr = ClassRegistration.objects.all()
+    #     self.assertEqual(len(cr), 2)
+    #     # TODO this needs to be completed
