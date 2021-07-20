b9871d128f1ae15b220a636c1f3a31541badab14
sam <sam.amundson@gmail.com>
2021-07-19 20:50:15 -0700

    trying to block sending to test emails

--- wpa_project/student_app/src/email.py
+++ wpa_project/student_app/src/email.py
@@ -13,12 +13,16 @@ class EmailMessage(EmailMultiAlternatives):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.from_email = settings.DEFAULT_FROM_EMAIL
+        self.test_emails = ["EmilyNConlan@einrot.com", "RosalvaAHall@superrito.com", "CharlesNWells@einrot.com",
+                            "RicardoRHoyt@jourrapide.com", "RicardoRHoyt@Ricardo.com"]
 
     def get_email_address(self, user):
         if settings.EMAIL_DEBUG:
             self.to = settings.EMAIL_DEBUG_ADDRESSES
         else:
             self.to = [EmailAddress.objects.get_primary(user)]
+            if self.to in self.test_emails:
+                self.to = settings.EMAIL_DEBUG_ADDRESSES
 
     def line_items(self, line_items):
         for line in line_items:
