c82667392d2dd60300930203f2aa5eedf63ef436
sam <sam.amundson@gmail.com>
2021-12-23 16:00:45 -0800

    Updated packages. Updated minutes.

--- wpa_project/payment/temp_tests.py
+++ wpa_project/payment/temp_tests.py
@@ -1,6 +1,5 @@
 # from .tests.tests_costs import TestsCosts
 # from .tests.tests_email import TestsEmail
-# from .tests.tests_payment import TestsPayment
-# from .tests.tests_process_payment import TestsPayment
+from .tests.tests_payment import TestsPayment
 # from .tests.tests_process_payment import TestsProcessPayment
 # from .tests.tests_square_helper import TestsSquareHelper
