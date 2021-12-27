ff1cd76242cda6f09a3037dc57502a790e102fb0
sam <sam.amundson@gmail.com>
2021-12-26 18:04:37 -0800

    Board can update membership with link in search results

--- wpa_project/membership/urls.py
+++ wpa_project/membership/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('level', LevelView.as_view(), name='level'),
     path('level_api', LevelApiView.as_view(), name='level_api'),
     path('membership', MembershipView.as_view(), name='membership'),
+    path('membership/<int:sf_id>', MembershipView.as_view(), name='membership'),
 ]
