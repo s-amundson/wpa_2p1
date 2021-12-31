aebac4abfb2f673f0d3bbeab008a890699655836
sam <sam.amundson@gmail.com>
2021-12-30 17:01:52 -0800

    Set up index to redirect to class registration if user is authenticated.

--- wpa_project/student_app/views/index_view.py
+++ wpa_project/student_app/views/index_view.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic.base import View
 from django.contrib import messages
+
 
 class IndexView(View):
     """Shows a message page"""
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect(reverse('programs:class_registration'))
         return render(request, 'student_app/index.html')
