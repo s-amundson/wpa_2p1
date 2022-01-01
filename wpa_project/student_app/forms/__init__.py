7bf7a653e45fffa3c3d752edab0cdceda896d1bd
sam <sam.amundson@gmail.com>
2022-01-01 15:12:49 -0800

    fix import error.

--- wpa_project/student_app/forms/__init__.py
+++ wpa_project/student_app/forms/__init__.py
@@ -2,6 +2,6 @@ from .instructor_form import InstructorForm
 from .signup_form import SignUpForm
 from .student_form import StudentForm
 from .student_family_registration_form import StudentFamilyRegistrationForm
-from .student_search_forms import SearchEmailForm, SearchNameForm, SearchPhoneForm
+from .student_search_forms import SearchColumnsForm, SearchEmailForm, SearchNameForm, SearchPhoneForm
 from .theme_form import ThemeForm
 from .user_form import UserForm
