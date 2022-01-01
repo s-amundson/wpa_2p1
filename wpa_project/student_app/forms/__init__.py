95652ac672d3f16ed31e3893cad66ba4b2c5aff4
sam <sam.amundson@gmail.com>
2022-01-01 14:35:23 -0800

    Updated search list to filter for instructors

--- wpa_project/student_app/forms/__init__.py
+++ wpa_project/student_app/forms/__init__.py
@@ -6,5 +6,5 @@ from .costs_form import CostsForm
 from .signup_form import SignUpForm
 from .student_form import StudentForm
 from .student_family_registration_form import StudentFamilyRegistrationForm
-from .student_search_forms import SearchEmailForm, SearchNameForm, SearchPhoneForm
+from .student_search_forms import SearchColumnsForm, SearchEmailForm, SearchNameForm, SearchPhoneForm
 from .theme_form import ThemeForm
