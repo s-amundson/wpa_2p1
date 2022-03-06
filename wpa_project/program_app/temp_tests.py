cdcbc4dad79e135e29ea133d34b17048372a0816
sam <sam.amundson@gmail.com>
2022-03-06 10:33:55 -0800

    Added admin registration log view.

--- wpa_project/program_app/temp_tests.py
+++ wpa_project/program_app/temp_tests.py
@@ -6,6 +6,7 @@
 # from .tests.tests_class_registration import TestsClassRegistration
 # from .tests.tests_class_registration_admin import TestsClassAdminRegistration
 # from .tests.tests_class_sign_in import TestsClassSignIn
+from .tests.tests_logs import TestsAdminRegistration
 # from .tests.tests_signal import TestsSignal
 # from .tests.tests_unregister import TestsUnregisterStudent
 # from .tests.tests_unregister import TestsUnregisterStudent2
---
+++ wpa_project/program_app/tests/tests_logs.py
@@ -0,0 +1,53 @@
+import logging
+
+from django.apps import apps
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from ..models import BeginnerClass, ClassRegistration, ClassRegistrationAdmin
+from student_app.models import Student
+
+logger = logging.getLogger(__name__)
+User = apps.get_model('student_app', 'User')
+
+
+class TestsAdminRegistration(TestCase):
+    fixtures = ['f1']
+
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        self.test_user = User.objects.get(pk=1)
+        self.client.force_login(self.test_user)
+        self.url = reverse('programs:class_registration_admin_list')
+
+    def test_user_normal_user_not_authorized(self):
+        self.test_user = User.objects.get(pk=3)
+        self.client.force_login(self.test_user)
+
+        response = self.client.get(self.url, secure=True)
+        self.assertEqual(response.status_code, 403)
+
+    def test_board_get_empty(self):
+        self.test_user = User.objects.get(pk=1)
+        self.client.force_login(self.test_user)
+
+        response = self.client.get(self.url, secure=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['object_list']), 0)
+
+    def test_board_get(self):
+        self.test_user = User.objects.get(pk=1)
+        self.client.force_login(self.test_user)
+        ncr = ClassRegistration.objects.create(beginner_class=BeginnerClass.objects.get(pk=1),
+                                               student=Student.objects.get(pk=4),
+                                               new_student=True,
+                                               pay_status='admin',
+                                               idempotency_key="7b16fadf-4851-4206-8dc6-81a92b70e52f")
+        note = 'test note'
+        cra = ClassRegistrationAdmin.objects.create(class_registration=ncr, staff=self.test_user, note=note)
+
+        response = self.client.get(self.url, secure=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['object_list']), 1)
+
