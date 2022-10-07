21b619f569f13c5dcabdcea3c7862bc106fd6ca8
sam <sam.amundson@gmail.com>
2022-10-07 14:09:59 -0700

    add captcha to signup form

--- wpa_project/student_app/forms/signup_form.py
+++ wpa_project/student_app/forms/signup_form.py
@@ -1,4 +1,5 @@
 from allauth.account.forms import SignupForm
+from captcha.fields import CaptchaField
 from django import forms
 from django.urls import reverse_lazy
 from django.utils.safestring import mark_safe
@@ -8,6 +9,7 @@ from django.utils.translation import gettext as _
 class SignUpForm(SignupForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['captcha'] = CaptchaField(required=True)
         terms = reverse_lazy("information:info", kwargs={'info': 'terms'})
         self.fields['terms'] = forms.BooleanField(widget=forms.CheckboxInput(
                 attrs={'class': "m-2"}), required=True,
