48685104d48eb4ee1f120674442f88947709bed4
s-amundson <sam.amundson@gmail.com>
2023-10-27 14:45:25 -0700

    Updating packages. Set python version to 3.11

--- wpa_project/wpa_project/settings.py
+++ wpa_project/wpa_project/settings.py
@@ -285,6 +285,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 PHONENUMBER_DB_FORMAT = 'NATIONAL'
