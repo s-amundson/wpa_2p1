ab0e1f7e0016f0a149fb4535fa2f31169ed121b4
s-amundson <sam.amundson@gmail.com>
2024-08-06 15:37:58 -0700

    modifying facebook posts to see what is causing the error.

--- wpa_project/facebook/urls.py
+++ wpa_project/facebook/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 app_name = 'facebook'
 urlpatterns = [
-    path('add_post', PostAddView.as_view(), name='add_post'),
+    # path('add_post', PostAddView.as_view(), name='add_post'),
     path('posts', PostList.as_view(), name='posts'),
     path('posts_include', PostListInsert.as_view(), name='posts_include')
 ]
