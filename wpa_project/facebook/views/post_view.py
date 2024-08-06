eec7c8613f1211699100f83442d9bbb42a717a3d
s-amundson <sam.amundson@gmail.com>
2024-08-06 15:43:19 -0700

    modifying facebook posts to see what is causing the error.

--- wpa_project/facebook/views/post_view.py
+++ wpa_project/facebook/views/post_view.py
@@ -1,35 +1,31 @@
 from django.views.generic import ListView
-from django.conf import settings
-from django.utils import timezone
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from src.mixin import BoardMixin
-from ..forms import PostForm
-from ..models import EmbeddedPosts, Posts
-from ..tasks import get_facebook_posts
-import requests
+# from django.urls import reverse_lazy
+# from django.views.generic.edit import CreateView
+# from src.mixin import BoardMixin
+# from ..forms import PostForm
+from ..models import Posts
+# from ..tasks import get_facebook_posts
+
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-class PostAddView(BoardMixin, CreateView):
-    model = EmbeddedPosts
-    form_class = PostForm
-    template_name = 'student_app/form_as_p.html'
-    success_url = reverse_lazy('facebook:posts')
+# class PostAddView(BoardMixin, CreateView):
+#     model = EmbeddedPosts
+#     form_class = PostForm
+#     template_name = 'student_app/form_as_p.html'
+#     success_url = reverse_lazy('facebook:posts')
 
 
 class PostList(ListView):
     paginate_by = 5
-    """
-    Return all posts that are with available true and order from the latest one.
-    """
     template_name = 'facebook/post_list.html'
 
     def get_queryset(self):
         logger.warning('facebook get queryset')
-        queryset = Posts.objects.filter(active=True).order_by('-created_time')
+        # queryset = Posts.objects.filter(active=True).order_by('-created_time')
+        queryset = Posts.objects.none()
         return queryset
 
     # def get_context_data(self, **kwargs):
