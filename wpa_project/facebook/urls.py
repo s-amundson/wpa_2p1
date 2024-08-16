4d5c1e11c86cf30f80bcb9ae9281b5f0a6134315
s-amundson <sam.amundson@gmail.com>
2024-08-16 07:12:33 -0700

    fixes for facebook callback view. I realized it didn't return anything and in doing some testing found a couple errors.

--- wpa_project/facebook/urls.py
+++ wpa_project/facebook/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 app_name = 'facebook'
 urlpatterns = [
-    path('page_update_hook', PageHook.as_view(), name='page_update_hook'),
-    path('posts', PostList.as_view(), name='posts'),
-    path('posts_include', PostListInsert.as_view(), name='posts_include'),
+    path('page_update_hook/', PageHook.as_view(), name='page_update_hook'),
+    path('posts/', PostList.as_view(), name='posts'),
+    path('posts_include/', PostListInsert.as_view(), name='posts_include'),
 ]
