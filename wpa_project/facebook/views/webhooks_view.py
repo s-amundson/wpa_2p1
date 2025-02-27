0c87af580cac92614f46e1a9602e4174aed07ec4
s-amundson <sam.amundson@gmail.com>
2025-02-26 19:28:21 -0800

    working on facebook intergration

--- wpa_project/facebook/views/webhooks_view.py
+++ wpa_project/facebook/views/webhooks_view.py
@@ -11,6 +11,10 @@ logger = logging.getLogger(__name__)
 
 @method_decorator(csrf_exempt, name="dispatch")
 class PageHook(View):
+    def get(self, request):
+        logger.warning(self.request.GET)
+        if 'hub.verify_token' in self.request.GET:
+            pass
     def post(self, request):
         logger.warning(self.request.POST)
         # TODO figure out how to process data to add posts to the website as they are posted to facebook.
