6f84814038a2c7705484efb5f2a86a00c4af341b
s-amundson <sam.amundson@gmail.com>
2025-03-02 11:13:55 -0800

    Updated election view to use a formset, removing the need for ElectionCandidateView. Also allowing to remove records with the formset when a candidate changes thier mind.

--- wpa_project/membership/forms/election_forms.py
+++ wpa_project/membership/forms/election_forms.py
@@ -21,26 +21,40 @@ class ElectionForm(MyModelForm):
         self.fields['election_date'].initial = timezone.now() + timezone.timedelta(days=30)
 
         self.fields['election_close'].widget = DateTimeInput()
+        self.fields['election_close'].required = False
         self.fields['notify'] = BooleanField(widget=CheckboxInput(
                 attrs={'class': "m-2"}), required=False, label="Send out notification email to members")
         if self.instance.id:
             logger.warning(self.instance.electioncandidate_set.all())
+        self.close_updated = False
 
+    def clean(self):
+        cleaned_data = super().clean()
+        logger.warning(cleaned_data)
+        if cleaned_data['election_close'] is None:
+            cleaned_data['election_close'] = cleaned_data['election_date'] + timezone.timedelta(days=1)
+            self.close_updated = True
+        return cleaned_data
 
 class ElectionCanidateForm(MyModelForm):
     class Meta(MyModelForm.Meta):
         model = ElectionCandidate
-        fields = ['election', 'position', 'student']
-        hidden_fields = []
+        required_fields = ['position', 'student']
+        hidden_fields = ['election', ]
         optional_fields = []
+        fields = required_fields + hidden_fields
 
     def __init__(self,  *args, **kwargs):
         super().__init__(*args, **kwargs)
-        d = self.initial['election'].election_date
+        if type(self.initial['election']) == int:
+            election = Election.objects.get(pk=self.initial['election'])
+        else:
+            election = self.initial['election']
+        d = election.election_date
         d = d.replace(year=d.year-18)
-        logger.warning(d)
         self.fields['student'].queryset = self.fields['student'].queryset.filter(
-            dob__lte=d, member__expire_date__gte=self.initial['election'].election_date)
+            dob__lte=d, member__expire_date__gte=election.election_date)
+        self.fields['election'].label = ""
 
 
 class ElectionVoteForm(MyModelForm):
