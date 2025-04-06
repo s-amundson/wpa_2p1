c3b4bd815f8680010dc1b285791e0e6024b0953e
s-amundson <sam.amundson@gmail.com>
2025-04-06 14:48:33 -0700

    added access levels to polices. update staff list.html to better fit images with paragraph. added function to tasks to update member status if missed on signal.

--- wpa_project/info/forms/policy_form.py
+++ wpa_project/info/forms/policy_form.py
@@ -1,12 +1,13 @@
 from src.model_form import MyModelForm
-from ..models import Policy, PolicyText
-from django.forms import ModelForm, CharField, CheckboxInput
+from ..models import Policy, PolicyText, ACCESS
+from django.forms import ModelForm, CharField, CheckboxInput, ChoiceField
+
 
 class PolicyForm(MyModelForm):
 
     class Meta(MyModelForm.Meta):
         model = Policy
-        required_fields = ['title']
+        required_fields = ['title', 'access']
         optional_fields = []
         fields = optional_fields + required_fields
 
@@ -23,6 +24,7 @@ class PolicyTextForm(MyModelForm):
         self.fields['title'].widget.attrs.update({'class': 'form-control m-2', 'style': 'display:none'})
         self.fields['title_text'] = CharField(label='Title')
         self.fields['title_text'].widget.attrs.update({'class': 'form-control m-2'})
+        self.fields['title_access'] = ChoiceField(label='Access', choices=ACCESS)
         self.fields['is_html'].widget = CheckboxInput()
         self.fields['is_html'].required = False
         if self.instance.id:
--- wpa_project/info/models/__init__.py
+++ wpa_project/info/models/__init__.py
@@ -2,5 +2,5 @@ from .annoucement_model import Announcement
 from .article_model import ArticlePage, Article
 from .category_model import Category
 from .faq_model import Faq
-from .policy_model import Policy, PolicyText
+from .policy_model import Policy, PolicyText, ACCESS
 from .staff_profile_model import StaffProfile
