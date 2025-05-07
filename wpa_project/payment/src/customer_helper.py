873f1674f1b283b05e48ab259db757ddd2945181
s-amundson <sam.amundson@gmail.com>
2025-05-07 14:46:10 -0700

    Revert packages2 (#204)

--- wpa_project/payment/src/customer_helper.py
+++ wpa_project/payment/src/customer_helper.py
@@ -1,6 +1,5 @@
 import uuid
 import logging
-from square.core.api_error import ApiError
 
 from ..models import Customer
 from .square_helper import SquareHelper
@@ -16,44 +15,60 @@ class CustomerHelper(SquareHelper):
 
     def create_customer(self):
         idempotency_key = str(uuid.uuid4())
-        try:
-            result = self.client.customers.create(
-                idempotency_key=idempotency_key,
-                email_address=self.user.email,
-            )
+        result = self.client.customers.create_customer(
+          body={
+            "idempotency_key": idempotency_key,
+            "email_address": self.user.email
+          }
+        )
+        if result.is_success():
+            # logging.debug(result.body)
+            customer = result.body['customer']
             self.customer = Customer.objects.create(
-                customer_id=result.customer.id,
-                created_at=result.customer.created_at,
-                creation_source=result.customer.creation_source,
-                updated_at=result.customer.updated_at,
+                customer_id=customer['id'],
+                created_at=customer['created_at'],
+                creation_source=customer['creation_source'],
+                updated_at=customer['updated_at'],
                 user=self.user,
-                version=result.customer.version
+                version=customer['version']
             )
             return self.customer
-        except ApiError as e:
-            self.handle_error(e, 'customers.create_customer', ik=idempotency_key)
+        elif result.is_error():  # pragma: no cover
+            for error in result.errors:
+                self.log_error('N/A', error.get('code', 'unknown_error'), idempotency_key, 'customers.create_customer')
+            self.handle_error(result, 'Customer create error')
         return None  # pragma: no cover
 
     def delete_customer(self):
         c = Customer.objects.get(user=self.user)
-        try:
-            result = self.client.customers.delete(customer_id=c.customer_id)
+        result = self.client.customers.delete_customer(
+            customer_id=c.customer_id,
+        )
+
+        if result.is_success():
             return True
-        except ApiError as e:
-            self.handle_error(e, 'customers.delete_customer')
+        elif result.is_error():  # pragma: no cover
+            for error in result.errors:
+                self.log_error('N/A', error.get('code', 'unknown_error'), None, 'customers.delete_customer')
+            self.handle_error(result, 'Customer delete error')
             return False
 
     def retrieve_customer(self):
         self.customer = Customer.objects.get(user=self.user)
-        try:
-            result = self.client.customers.get(customer_id=self.customer.customer_id)
+        result = self.client.customers.retrieve_customer(
+            customer_id=self.customer.customer_id,
+        )
 
-            self.customer.created_at = result.customer.created_at
-            self.customer.creation_source = result.customer.creation_source
-            self.customer.updated_at = result.customer.updated_at
+        if result.is_success():
+            customer = result.body['customer']
+            self.customer.created_at = customer['created_at']
+            self.customer.creation_source = customer['creation_source']
+            self.customer.updated_at = customer['updated_at']
             self.customer.user = self.user
-            self.customer.version = result.customer.version
+            self.customer.version = customer['version']
             return self.customer
-        except ApiError as e:
-            self.handle_error(e, 'customers.retrieve_customer')
+        elif result.is_error():  # pragma: no cover
+            for error in result.errors:
+                self.log_error('N/A', error.get('code', 'unknown_error'), None, 'customers.retrieve_customer')
+            self.handle_error(result, 'Customer retrieve error')
         return None  # pragma: no cover
