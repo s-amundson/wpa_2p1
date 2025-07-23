be3c1eb78b190b713eedf633a3c3b60e470a486a
s-amundson <sam.amundson@gmail.com>
2025-07-22 20:35:10 -0700

    restrict awards to non board members.

--- wpa_project/contact_us/models/compaint_model.py
+++ wpa_project/contact_us/models/compaint_model.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 class Complaint(models.Model):
     category_choices = [('services', 'Services'), ('harassment', 'Harassment'), ('website', 'Website'),
-                        ('staff', 'Staff Issues'), 'maintenance', 'Range Maintenance']
+                        ('staff', 'Staff Issues'), ('maintenance', 'Range Maintenance')]
     category = models.CharField(max_length=20, null=True, choices=category_choices)
     created_time = models.DateTimeField(default=timezone.now)
     incident_date = models.DateField(null=True, default=None)
